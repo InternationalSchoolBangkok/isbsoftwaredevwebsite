0b386200c434eebf1aba813c6fccf829507dccf0
Nico <ramicaza@gmail.com>
2016-02-02 14:29:24 +0700

    dunno

--- contact.php
+++ contact.php
@@ -3,9 +3,9 @@
 <head>
 	<title>Software Development</title>
 	<?php include 'common1.php'; ?>
-	<link rel="stylesheet" type="text/css" href="css/contact.css">
+	<link rel='stylesheet' type='text/css' href='css/contact.css'>
+	<link rel='stylesheet' type='text/css' href='css/form.css'>
 	<script src="js/contact.js?1"></script>
-	<link rel="stylesheet" type="text/css" href="css/form.css">
 </head>
 <body style="display: block;">
 	<div class="content">
